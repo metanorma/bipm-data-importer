df9f4efcf443a9f84b8024a5a04549157bfc6a1d
webdev778 <webdev778@gmail.com>
2022-08-30 07:08:57 +0900

    Add a viewer for outcomes

--- spec/bipm/data/importer/cgpm_spec.rb
+++ spec/bipm/data/importer/cgpm_spec.rb
@@ -4,7 +4,6 @@ RSpec.describe "data" do
   $all_files = Dir[File.join(__dir__, '../../../../data/cgpm/meetings-*/*.yml')]
 
   $all_data = $all_files.map do |f|
-    puts f
     YAML.load_file(Pathname.new(f).expand_path.to_s)
   end
 
@@ -12,10 +11,10 @@ RSpec.describe "data" do
     i["resolutions"]
   end.flatten
   $all_texts = $all_resolutions.map do |r|
-    [ r["approvals"].map { |j| j["message"] } ] +
-    [ r["considerations"].map { |j| j["message"] } ] +
-    [ r["actions"].map { |j| j["message"] } ]
-  end.flatten
+    [ r["approvals"]&.map { |j| j["message"] } ] +
+    [ r["considerations"]&.map { |j| j["message"] } ] +
+    [ r["actions"]&.map { |j| j["message"] } ]
+  end.flatten.compact
   $all_stems = $all_texts.map do |i|
     i.scan(/stem:\[([^\]]*)\]/)
   end.flatten
---
+++ spec/bipm/data/outcomes_spec.rb
@@ -0,0 +1,23 @@
+# frozen-string-literal: true
+
+RSpec.describe Bipm::Data::Outcomes do
+  it "acts like an array of Bodies" do
+    expect(described_class.to_a.map(&:class)).to eq [described_class::Body] * 3
+  end
+
+  describe described_class::Body do
+    let(:instance) { Bipm::Data::Outcomes.to_a.first }
+
+    it "acts like an array of LocalizedBodies" do
+      expect(instance.to_a.first.class).to be Bipm::Data::Outcomes::LocalizedBody
+    end
+  end
+
+  describe described_class::LocalizedBody do
+    let(:instance) { Bipm::Data::Outcomes.to_a.first.to_a.first }
+
+    it "acts like an array of Meetings" do
+      expect(instance.to_a.first.class).to be Bipm::Data::Outcomes::Meeting
+    end
+  end
+end
