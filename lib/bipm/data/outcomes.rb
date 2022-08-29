e9156c71c99fe3843c76ad271af86de07969d9a3
webdev778 <webdev778@gmail.com>
2022-08-30 07:23:25 +0900

    Automatically download data if not present and path not specified

--- lib/bipm/data/outcomes.rb
+++ lib/bipm/data/outcomes.rb
@@ -6,10 +6,12 @@ module Bipm
   module Data
     module Outcomes
       def self.file_path
-        "#{__dir__}/../../../data/"
+        @file_path ||= "#{__dir__}/../../../data/"
       end
+      singleton_class.attr_writer :file_path
 
       def self.body(name)
+        ensure_downloaded
         Body.new(name)
       end
       singleton_class.alias_method :[], :body
@@ -31,6 +33,20 @@ module Bipm
       autoload :Approval, "bipm/data/outcomes/approval"
       autoload :Consideration, "bipm/data/outcomes/consideration"
       autoload :Action, "bipm/data/outcomes/action"
+
+      # It may be possible, that we don't have the BIPM data loaded.
+      def self.ensure_downloaded
+        if File.exist?(file_path)
+          return
+        elsif !File.writable?(File.dirname(file_path))
+          require "fileutils"
+          self.file_path = "#{ENV["HOME"]}/.local/share/metanorma/bipm-data-importer/data/"
+          FileUtils.mkdir_p(File.dirname(self.file_path))
+        end
+        git_path = "https://github.com/metanorma/bipm-data-outcomes"
+        system "git", "clone", git_path, file_path or
+          raise "Downloading bipm-data-outcomes failed: possibly git not available"
+      end
     end
   end
 end
