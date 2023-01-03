5fd9bc8e2ff20a565e04405cc8d1f6fe8199bb60
webdev778 <webdev778@gmail.com>
2023-01-03 16:57:23 +0900

    JCRB/fr 43 had repeated identifiers
    This fixes metanorma/bipm-data-outcomes#17

--- spec/bipm/data/importer/unique_idents_spec.rb
+++ spec/bipm/data/importer/unique_idents_spec.rb
@@ -8,13 +8,6 @@ RSpec.describe "data" do
           describe lbody.locale do
             lbody.meetings.each_value do |meeting|
               describe meeting.id do
-                # Mistake on a website: numbering is wrong
-                # https://www.bipm.org/fr/committees/jc/jcrb/43-2021
-                if [body.body, lbody.locale, meeting.id] == [:jcrb, :fr, "43"]
-                  xit "has unique resolution ids"
-                  next
-                end
-
                 it "has unique resolution ids" do
                   resolutions = meeting.resolutions.values.map{|i| [i.type,i.id] }.sort
                   unique_resolutions = resolutions.uniq
